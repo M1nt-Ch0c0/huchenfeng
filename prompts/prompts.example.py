a3642530003fb9391a102444316b6883cd2466df
tingz <tinkinzhang@gmail.com>
2025-11-26 18:05:47 +0800

    init

---
+++ prompts/prompts.example.py
@@ -0,0 +1,5 @@
+
+
+if __name__ == "__main__":
+    from prompts import render_denoise_and_split
+    print(render_denoise_and_split(text="测试文本"))
---
+++ utils.py
@@ -0,0 +1,4 @@
+# TODO:规整化 + 去噪 + 分段
+def denoise_and_split_text(text:str)->list[str]:
+    pass
+    
